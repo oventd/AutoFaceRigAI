28401fbe302e924e7046f8816adab567eafa0889
OvenTD <155340997+OvenTD@users.noreply.github.com>
2025-11-11 20:44:29 +0900

    tracker group function

--- tracker.py
+++ tracker.py
@@ -12,7 +12,7 @@ class Tracker:
         self._sample_rate = SampleRate()
         self._group = "grp_tracker"
 
-        self._group = cmds.group(name=self._group, em=True)
+        self._create_group()
 
     @property
     def head(self):
@@ -34,22 +34,30 @@ class Tracker:
     def sample_rate(self, value=10, x=None, y=None):
         self._sample_rate.x = value if x is None else x
         self._sample_rate.y = value if y is None else y
+        
+    def _create_group(self):
+        self.delete_group()
+        self._group = cmds.group(name=self._group, em=True)
+        cmds.select(clear=True)
 
-    def init_head(self):
-        self.head.init()
-    
+    def delete_group(self):
+        if cmds.objExists(self.group):
+            cmds.delete(self.group)
+   
     def create_head_bbox(self):
         self.head.create_bbox()
-        cmds.parent(self.head.bbox_group, self.group)
+        self._parent(self.head.bbox_group)
 
     def create_camera(self):
         self._camera_creator.create(
             target=self.head.bbox,
             group_position=self.head.transition)
         self.head.hide_bbox()
-        
-        cmds.parent(self._camera_creator.group, self.group)
 
-    
+        self._parent(self._camera_creator.group)
+
+    def _parent(self, child):
+        cmds.parent(child, self.group)
+        cmds.select(clear=True)
     
 
